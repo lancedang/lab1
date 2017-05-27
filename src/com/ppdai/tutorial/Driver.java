5d5c0505e452b74a1ad0a66b260f4ab57ed32cbd
qiankai02 <qiankai02@ppdai.com>
2017-05-27 14:22:11 +0800

    Add trim method to remove the blankspace

--- src/com/ppdai/tutorial/Driver.java
+++ src/com/ppdai/tutorial/Driver.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 /**
  * PPdai java tutorial
+ * 
  * @author dangdang
  * @version 2017.5.20
  *
@@ -13,7 +14,7 @@ import java.io.InputStreamReader;
 public class Driver {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("hello world");
+		// System.out.println("hello world");
 		// TODO Auto-generated method stub
 		BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(System.in));
--- src/com/ppdai/tutorial/Planning.java
+++ src/com/ppdai/tutorial/Planning.java
@@ -2,6 +2,7 @@ package com.ppdai.tutorial;
 
 /**
  * Planning is a class including class info
+ * 
  * @author dangdang
  * @version 2017.5.20
  */
@@ -10,23 +11,25 @@ public class Planning {
 
 	/**
 	 * Constructor
+	 * 
 	 * @param str
 	 */
 	public Planning(String str) {
 		String[] resultStrings = str.split(",");
-		info[0] = resultStrings[0];
-		info[1] = resultStrings[1];
-		info[2] = resultStrings[2];
+
+		info[0] = resultStrings[0].trim();
+		info[1] = resultStrings[1].trim();
+		info[2] = resultStrings[2].trim();
 		info[3] = ""
-				+ (Integer.parseInt(resultStrings[1])
-						+ Integer.parseInt(resultStrings[2]) - 1)
-				/ Integer.parseInt(resultStrings[2]);
+				+ (Integer.parseInt(info[1]) + Integer.parseInt(info[2]) - 1)
+				/ Integer.parseInt(info[2]);
 
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "CLASS: "+info[0] + ", Enrolled: "+info[1] + ", Max class size: " + info[2] + ", Sections: " +info[3];
+		return "CLASS: " + info[0] + ", Enrolled: " + info[1]
+				+ ", Max class size: " + info[2] + ", Sections: " + info[3];
 	}
 }
